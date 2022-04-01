694b344d8e46a825204ef3a6c39bbb6fbffdc5ce
Bricktech <bricktech2000@gmail.com>
2022-04-01 19:18:43 -0400

    basic replacement with color

--- script.js
+++ script.js
@@ -8,16 +8,22 @@ window.addEventListener('load', async () => {
 
     console.log('replacing keys...');
 
-    replaceOnDocument(Object.keys(json), 'PROF NAME REPLACED');
+    replaceOnDocument(
+      Object.keys(json),
+      Object.values(json).map((value) => value.s / 5)
+    );
 
     console.log('done.');
   });
 });
 
+// https://stackoverflow.com/questions/22015684/how-do-i-zip-two-arrays-in-javascript
+const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
 // https://stackoverflow.com/questions/18643766/find-and-replace-specific-text-characters-across-a-document-with-js
 const replaceOnDocument = (
   patterns,
-  string,
+  strings,
   { target = document.body } = {}
 ) => {
   // Handle `string` — see the last section
@@ -28,11 +34,16 @@ const replaceOnDocument = (
     nodes
       .filter(({ nodeType }) => nodeType === document.TEXT_NODE)
       .forEach((textNode) => {
-        patterns.forEach((pattern) => {
-          textNode.textContent = textNode.textContent.replaceAll(
-            pattern,
-            string
-          );
+        zip(patterns, strings).forEach(([pattern, string]) => {
+          if (textNode.textContent.includes(pattern)) {
+            textNode.parentNode.style.backgroundColor = `hsl(${
+              string * (360 / 3)
+            }, 100%, 50%)`;
+          }
+          // textNode.textContent = textNode.textContent.replaceAll(
+          //   pattern,
+          //   string
+          // );
         });
       })
   );
