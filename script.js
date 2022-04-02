08cb637890befe2c1cdf0c99f8acc78065d7d300
Bricktech <bricktech2000@gmail.com>
2022-04-01 20:47:58 -0400

    debounced function and enabled before window load

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
-window.addEventListener('load', async () => {
-  console.log('extension loaded.');
+console.log('extension loaded.');
 
-  console.log('loading data...');
-  chrome.runtime.sendMessage({}, async (url) => {
-    const res = await fetch(url);
-    const json = await res.json();
+console.log('loading data...');
+chrome.runtime.sendMessage({}, async (url) => {
+  const res = await fetch(url);
+  const json = await res.json();
 
-    const run = async ({ target = document.body } = {}) => {
-      // console.log('waiting...');
-      // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const run = async ({ target = document.body } = {}) => {
+    // console.log('waiting...');
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      console.log('replacing keys...');
+    console.log('replacing keys...');
 
-      replaceOnDocument(
-        Object.keys(json),
-        Object.entries(json).map(
-          ([key, value]) =>
-            `<span id="RateMyProfID" style="text-decoration: underline solid hsl(${
-              (value.s / 5) * (360 / 3)
-            }, 100%, 50%) 2px;">${key}</span>`
-        ),
-        { target }
-      );
-      console.log('done.');
-    };
+    replaceOnDocument(
+      Object.keys(json),
+      Object.entries(json).map(
+        ([key, value]) =>
+          `<span id="RateMyProfID" style="text-decoration: underline solid hsl(${
+            (value.s / 5) * (360 / 3)
+          }, 100%, 50%) 2px;">${key}</span>`
+      ),
+      { target }
+    );
+    console.log('done.');
+  };
 
-    // https://blog.sessionstack.com/how-javascript-works-tracking-changes-in-the-dom-using-mutationobserver-86adc7446401
-    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/MutationObserver
+  const debouncedRun = debounce(run);
 
-    const mutationObserver = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        // https://stackoverflow.com/questions/6518802/check-if-element-is-a-div
-        if (
-          mutation.target.id !== 'RateMyProfID' &&
-          mutation.target.tagName !== '' // text node (maybe?)
-        )
-          run({ target: mutation.target });
-      });
+  // https://blog.sessionstack.com/how-javascript-works-tracking-changes-in-the-dom-using-mutationobserver-86adc7446401
+  // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+  // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/MutationObserver
+
+  const mutationObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      // https://stackoverflow.com/questions/6518802/check-if-element-is-a-div
+      if (
+        mutation.target.id !== 'RateMyProfID' &&
+        mutation.target.tagName !== '' // text node (maybe?)
+      )
+        debouncedRun();
     });
-
-    mutationObserver.observe(document, {
-      childList: true,
-      subtree: true,
-    });
-
-    document.body.addEventListener('click', run);
-    run();
   });
+
+  mutationObserver.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+
+  debouncedRun();
 });
+
+// https://www.freecodecamp.org/news/javascript-debounce-example/
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
 
 // https://stackoverflow.com/questions/22015684/how-do-i-zip-two-arrays-in-javascript
 const zip = (a, b) => a.map((k, i) => [k, b[i]]);
